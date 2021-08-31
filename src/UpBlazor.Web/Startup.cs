782a5ed37ed4cc154f6b8a725336347ddb315e0b
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-08-31 15:07:14 +1000

    Add AntDesign + Charts

--- src/UpBlazor.Web/Startup.cs
+++ src/UpBlazor.Web/Startup.cs
@@ -22,6 +22,7 @@ namespace UpBlazor.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddAntDesign();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

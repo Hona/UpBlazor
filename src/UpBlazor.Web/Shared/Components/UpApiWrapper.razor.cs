b1d96fb1dfb9651c740ba60e2626b021c07bd156
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-01 16:29:31 +1000

    Abstract handling API calls in the UI

---
+++ src/UpBlazor.Web/Shared/Components/UpApiWrapper.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Up.NET.Api;
+using Up.NET.Models;
+
+namespace UpBlazor.Web.Shared.Components
+{
+    // TODO: Move this to @typeparam when .NET 6 is out
+    public partial class UpApiWrapper<T> : ComponentBase where T : class
+    {
+
+    }
+}

27db6f600fc9acaf15e270c2de50b83bd547ca3b
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-02 10:53:51 +1000

    Add repository wrapper

---
+++ src/UpBlazor.Web/Shared/Components/RepositoryWrapper.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UpBlazor.Web.Shared.Components
+{
+    public partial class RepositoryWrapper<TRepository, TResponse> : ComponentBase where TRepository : class
+    {
+        
+    }
+}

d17a386e4fe9efbb49a3f23dd143a11f51bfe122
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-02 10:10:18 +1000

    Make TwoUp ID sorted

--- src/UpBlazor.Core/Models/TwoUp.cs
+++ src/UpBlazor.Core/Models/TwoUp.cs
@@ -1,8 +1,19 @@
-﻿namespace UpBlazor.Core.Models
+﻿using System.Linq;
+
+namespace UpBlazor.Core.Models
 {
     public class TwoUp
     {
-        public string MartenId => UserId1 + UserId2;
+        public string MartenId
+        {
+            get
+            {
+                // Ensure that there are not duplicate entries (e.g. (1,2) and (2,1))
+                var userIds = new []{ UserId1, UserId2};
+                return string.Join(string.Empty, userIds.OrderBy(x => x));
+            }
+        }
+
         public string UserId1 { get; set; }
         public string UserId2 { get; set; }
     }

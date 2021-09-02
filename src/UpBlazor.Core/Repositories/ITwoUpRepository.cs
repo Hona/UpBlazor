51f451d128ca5e02dc63f62ef21a2bc36800a373
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-02 10:41:28 +1000

    Scaffold 2Up navigation and infra

---
+++ src/UpBlazor.Core/Repositories/ITwoUpRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UpBlazor.Core.Models;
+
+namespace UpBlazor.Core.Repositories
+{
+    public interface ITwoUpRepository : IGenericRepository<TwoUp>
+    {
+        Task<IReadOnlyList<TwoUp>> GetAllByUserIdAsync(string userId);
+        Task<TwoUp> GetByBothUserIdsAsync(string userId1, string userId2);
+    }
+}
---
+++ src/UpBlazor.Infrastructure/Repositories/TwoUpRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Marten;
+using UpBlazor.Core.Models;
+using UpBlazor.Core.Repositories;
+
+namespace UpBlazor.Infrastructure.Repositories
+{
+    public class TwoUpRepository : GenericRepository<TwoUp>, ITwoUpRepository
+    {
+        protected TwoUpRepository(IDocumentStore store) : base(store) { }
+        
+        public async Task<IReadOnlyList<TwoUp>> GetAllByUserIdAsync(string userId)
+        {
+            using var session = Store.QuerySession();
+
+            return await session.Query<TwoUp>()
+                .Where(x => x.UserId1 == userId || x.UserId2 == userId)
+                .ToListAsync();
+        }
+
+        public async Task<TwoUp> GetByBothUserIdsAsync(string userId1, string userId2)
+        {
+            using var session = Store.QuerySession();
+
+            return await session.Query<TwoUp>()
+                .SingleOrDefaultAsync(x => x.UserId1 == userId1 && x.UserId2 == userId2 ||
+                                           x.UserId1 == userId2 && x.UserId2 == userId1);
+        }
+    }
+}

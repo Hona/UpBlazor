aa9b60c90b2640f297a99c53d9b1350fde8a58ca
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-03 16:50:09 +1000

    Scaffold Budget frontend

---
+++ src/UpBlazor.Core/Models/ExpenseAggregate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace UpBlazor.Core.Models
+{
+    public class ExpenseAggregate
+    {
+        public IReadOnlyList<Expense> Expenses { get; set; }
+        public IReadOnlyList<RecurringExpense> RecurringExpenses { get; set; }
+    }
+}

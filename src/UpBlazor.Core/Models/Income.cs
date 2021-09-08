0de93f4a7d458fa106b07718fe80c116f1a03c69
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-08 10:30:43 +1000

    Create income page

--- src/UpBlazor.Core/Models/Income.cs
+++ src/UpBlazor.Core/Models/Income.cs
@@ -7,6 +7,8 @@ namespace UpBlazor.Core.Models
     {
         public Guid Id { get; set; }
         public string UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
         public Money Money { get; set; }
         public Interval Interval { get; set; }
         public int IntervalUnits { get; set; }

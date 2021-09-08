36c100b135b27b6ca44c779ef758de9e125aac8b
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-08 15:55:27 +1000

    Implement basic planner

---
+++ src/UpBlazor.Web/ViewModels/IncomeGoalRunningTotal.cs
@@ -0,0 +1,19 @@
+﻿using UpBlazor.Core.Models;
+
+namespace UpBlazor.Web.ViewModels
+{
+    public class IncomeGoalRunningTotal : IncomeGoal
+    {
+        public IncomeGoalRunningTotal(IncomeGoal incomeGoal, decimal runningTotal)
+        {
+            Id = incomeGoal.Id;
+            Amount = incomeGoal.Amount;
+            Name = incomeGoal.Name;
+            IncomeId = incomeGoal.IncomeId;
+            SaverId = incomeGoal.SaverId;
+            
+            RunningTotal = runningTotal;
+        }
+        public decimal RunningTotal { get; set; }
+    }
+}

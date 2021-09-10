27594d172255046a091a1ef0b0047a5114feacc7
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-10 10:12:26 +1000

    Refactor cascading parameter names to a constant file

---
+++ src/UpBlazor.Web/CascadingParameters/CascadingConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace UpBlazor.Web.CascadingParameters
+{
+    public static class CascadingConstants
+    {
+        public const string UpApiWrapper = "UpApiContext";
+        public const string RepositoryWrapper = "RepositoryContext";
+        public const string UserIdWrapper = "UserId";
+        public const string NormalizedAggregateWrapper = "NormalizedAggregate";
+    }
+}

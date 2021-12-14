e57119de95a4d87dbf6b4ab89bf191319de2ee7e
Luke Parker [SSW] <10430890+Hona@users.noreply.github.com>
2021-12-14 15:26:46 +1000

    ✨ Add read receipts

---
+++ src/UpBlazor.Application/Features/Notifications/GetAllWhoReadNotificationQuery.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using UpBlazor.Core.Models;
+using UpBlazor.Core.Repositories;
+
+namespace UpBlazor.Application.Features.Notifications;
+
+public record GetAllWhoReadNotificationQuery(Notification Notification) : IRequest<IReadOnlyList<RegisteredUser>>;
+
+public class GetAllWhoReadNotificationQueryHandler : IRequestHandler<GetAllWhoReadNotificationQuery, IReadOnlyList<RegisteredUser>>
+{
+    private readonly IRegisteredUserRepository _registeredUserRepository;
+    private readonly INotificationReadRepository _notificationReadRepository;
+
+    public GetAllWhoReadNotificationQueryHandler(IRegisteredUserRepository registeredUserRepository, INotificationReadRepository notificationReadRepository)
+    {
+        _registeredUserRepository = registeredUserRepository;
+        _notificationReadRepository = notificationReadRepository;
+    }
+
+    public async Task<IReadOnlyList<RegisteredUser>> Handle(GetAllWhoReadNotificationQuery request, CancellationToken cancellationToken)
+    {
+        var notificationId = request.Notification.Id;
+        var readReceipts = await _notificationReadRepository.GetByNotificationIdAsync(notificationId, cancellationToken);
+
+        var userIds = readReceipts
+            .Select(x => x.UserId)
+            .ToArray();
+
+        var output = await _registeredUserRepository.GetAllByIdsAsync(cancellationToken, userIds);
+        return output;
+    }
+}

c0feda99321408fdb87ff9352dc046fd72a57785
Luke Parker [SSW] <10430890+Hona@users.noreply.github.com>
2023-03-26 10:09:29 +1000

    rip out api client di from UI

---
+++ src/UpBlazor.ApiClient/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace UpBlazor.ApiClient;
+
+public static class DependencyInjection
+{
+    public static void AddApiClients(this IServiceCollection services, string namedHttpClient)
+    {
+        services.AddHttpClient<ExpensesClient>(namedHttpClient);
+        services.AddHttpClient<ForecastClient>(namedHttpClient);
+        services.AddHttpClient<IncomesClient>(namedHttpClient);
+        services.AddHttpClient<NormalizedClient>(namedHttpClient);
+        services.AddHttpClient<NotificationsClient>(namedHttpClient);
+        services.AddHttpClient<UpClient>(namedHttpClient);
+        services.AddHttpClient<PlannerClient>(namedHttpClient);
+        services.AddHttpClient<UsersClient>(namedHttpClient);
+        services.AddHttpClient<RecurringExpensesClient>(namedHttpClient);
+        services.AddHttpClient<SavingsPlanClient>(namedHttpClient);
+    }
+}
